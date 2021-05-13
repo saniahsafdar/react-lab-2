60f9fe10e8cf0cad21e362fdd8e4afc6daca155b
Saniah Safdar <77857761+saniahsafdar@users.noreply.github.com>
2021-05-13 17:16:15 -0400

    Update SocialPosts.tsx

--- src/components/SocialPosts.tsx
+++ src/components/SocialPosts.tsx
@@ -1,14 +1,25 @@
 import React from "react";
+import {useState} from 'react'
 import PostForm from "./PostForm";
 
 function SocialPosts() {
+    const [postform, setPostForm] = useState(false);
+ 
+    function showPostForm(postform: PostForm){
+        
+        setPostForm(true);
+    }
 
     return (
         <div className="SocialPosts">
-            <PostForm />
+            <div style={{display:"none"}}> <PostForm setPostForm={true} /></div>
+
+            <button onClick = {()=>setPostForm(true)} >New Thought</button>
         </div>
     )
 }
+
+{/* <button disabled={theme === false} onClick={dark}>Light</button> */}
 
 
 export default SocialPosts;
